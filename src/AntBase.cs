95d184c11115c8b834167d6bb15cb507876ed5e3
xwedea <mecitjiyan@gmail.com>
2023-07-07 19:12:34 -0400

    movement to mouse click

--- src/AntBase.cs
+++ src/AntBase.cs
@@ -3,37 +3,49 @@ using System;
 
 public partial class AntBase : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 120;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public Vector2 TargetLocation;
+	public bool HaveTargetLocation = false;
+
+
+	public override void _Ready()
+	{
+		base._Ready();
+		
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		if (Position.DistanceTo(TargetLocation) < 1) {
+			HaveTargetLocation = false;
 		}
 
-		Velocity = velocity;
-		MoveAndSlide();
+		if (HaveTargetLocation) {
+			float distance = Position.DistanceTo(TargetLocation);
+			Vector2 direction = Position.DirectionTo(TargetLocation);
+
+			if (distance < Speed) {
+				Velocity = direction * distance;
+			}
+			Velocity = direction * Speed;
+
+
+			MoveAndSlide();
+		}
+
+
 	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton eventMouseButton) {
+			TargetLocation = eventMouseButton.GlobalPosition;
+			HaveTargetLocation = true;
+		}
+
+
+	}
+
+	
 }

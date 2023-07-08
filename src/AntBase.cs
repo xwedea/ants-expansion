6cd05e6de141961248e9b344010125efa1670e35
xwedea <mecitjiyan@gmail.com>
2023-07-08 12:16:49 -0400

    QueenAnt class

--- src/AntBase.cs
+++ src/AntBase.cs
@@ -20,47 +20,71 @@ public partial class AntBase : CharacterBody2D
 
 	AntState AnimState = AntState.Idle;
 
-	public const float Speed = 120;
+	public const float VelocityMultiplier = 12000;
 	
 	public override void _Ready()
 	{
 		base._Ready();
 
 		AnimPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		AnimPlayer.Play("idle");
+
 		NavAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
 	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
-		if (NavAgent.IsNavigationFinished()) return;
+		float velSize = Velocity.Length();
+
+		if (velSize > 0) {
+			ToWalking();
+		}
+		if (velSize == 0) {
+			ToIdle();
+		}
+
+		if (NavAgent.IsNavigationFinished()) {
+			Velocity = Vector2.Zero;
+			return;
+		}
 
 		Vector2 nextPosition = NavAgent.GetNextPathPosition();
 		Vector2 direction = Position.DirectionTo(nextPosition);
 
-		Velocity = direction * Speed;
+		Velocity = direction * VelocityMultiplier * (float)delta;
 		MoveAndSlide();
 		
 	}
 
 	public override void _Process(double delta)
 	{
-		if (Velocity.Length() > 5) {
-			ToWalking();
-		}
-		
-		if (Velocity.Length() < 1) {
-			ToIdle();
-		}
+		// if (Input.IsActionJustPressed("Enter")) {
+		// 	String currentAnim = AnimPlayer.CurrentAnimation;
+		// 	GD.Print(currentAnim);
+
+		// 	if (currentAnim == "idle") {
+		// 		ToWalking();
+		// 	}
+		// 	else if (currentAnim == "walk") {
+		// 		ToIdle();
+		// 	}
+		// }
 
 	}
 
 	public void ToIdle() {
 		AnimState = AntState.Idle;
-		AnimPlayer.Play("idle");
+
+		String currentAnim = AnimPlayer.CurrentAnimation;
+		if (currentAnim != "idle")
+			AnimPlayer.Play("idle");
 	}
 	public void ToWalking() {
 		AnimState = AntState.Walking;
-		AnimPlayer.Play("walk");
+
+		String currentAnim = AnimPlayer.CurrentAnimation;
+		if (currentAnim != "walk")
+			AnimPlayer.Play("walk");
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
--- src/QueenAnt.cs
+++ src/QueenAnt.cs
@@ -4,24 +4,16 @@ using System.Collections.Generic;
 
 public partial class QueenAnt : AntBase
 {
-		
+
+	int FoodStock;
+	
 	public override void _Ready()
 	{
 		base._Ready();
 
 	}
 	
-	public override void _PhysicsProcess(double delta)
-	{
-		if (NavAgent.IsNavigationFinished()) return;
-
-		Vector2 nextPosition = NavAgent.GetNextPathPosition();
-		Vector2 direction = Position.DirectionTo(nextPosition);
-
-		Velocity = direction * Speed;
-		MoveAndSlide();
-		
-	}
+	
 
 
 }
